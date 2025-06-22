974d322e462b71c7770bbc7178a50f6aef67eba3
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-06-21 18:17:35 -0600

    [Major] Setup Player
    + Setup player and state machine
    + Started some game control scripts.
    + Added walking / idle state

---
+++ scenes/Player/states/state_machine.gd
@@ -0,0 +1,57 @@
+class_name StateMachine extends Node
+
+@export var starting_state: State
+
+var _state_list:Array[State]
+var get_state_list:Array[State]:
+	get:
+		return _state_list
+
+var _current_state: State = null
+var get_current_state: State:
+	get:
+		return _current_state
+
+var _last_state: State = null
+var get_last_state: State:
+	get:
+		return _last_state
+
+var _next_state: State = null
+var get_next_state: State:
+	get:
+		return _next_state
+
+func init(parent: Player, animation_player: AnimationPlayer = null) -> void:
+	for child in get_children():
+		if child is State:
+			_state_list.push_back(child)
+			child._set_params(self, parent, animation_player)
+	
+	if not starting_state:
+		starting_state = _state_list[0]
+	change_state(starting_state)
+
+func change_state(new_state: State) -> void:
+	if not new_state:
+		push_error("Current State: %s NOT FOUND" % new_state.name)
+	
+	_next_state = new_state
+	
+	if _current_state:
+		_current_state.exit()
+	
+	_last_state = _current_state
+	_next_state = null
+	
+	_current_state = new_state
+	_current_state.enter()
+
+func process_input(event: InputEvent) -> void:
+	_current_state.process_input(event)
+
+func process_physics(delta: float) -> void:
+	_current_state.process_physics(delta)
+
+func process_frame(delta: float) -> void:
+	_current_state.process_frame(delta)
---
+++ scenes/Player/walk.gd
@@ -0,0 +1,25 @@
+extends State
+
+func process_physics(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if direction == Vector2.ZERO:
+		state_return(&"Idle")
+	
+	if direction.x > 0.1:
+		set_animation(&"HorizontalWalk")
+		parent.flip_player(false)
+	elif direction.x < -0.1:
+		set_animation(&"HorizontalWalk")
+		parent.flip_player(true)
+	elif direction.y < -0.1:
+		set_animation(&"UpWalk")
+		parent.flip_player(false)
+	elif direction.y > 0.1:
+		set_animation(&"DownWalk")
+		parent.flip_player(false)
+	
+	parent.move(direction)
+
+func set_animation(animation: StringName) -> void:
+	animation_player.play(animation)
