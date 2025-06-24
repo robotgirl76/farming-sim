39de6109d03a9e0ba777f4b318001de1e3466264
Powder Valentine <wildfire0502@gmail.com>
2025-06-23 23:01:16 -0400

    bed time

--- scenes/Player/attack.gd
+++ scenes/Player/attack.gd
@@ -3,12 +3,6 @@ extends State
 func process_frame(_delta: float) -> void:
 	parent.animation_component.attack_animation()
 
-
-
-
-
-
-
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == &"attack_up" or &"attack_down" or &"attack_left" or &"attack_right":
 		state_return(&"Idle")
