66bf85e877c64077adcf23fb5cba9efee3caf709
Powder Valentine <wildfire0502@gmail.com>
2025-06-25 23:24:54 -0400

    added the music I was working on

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -7,7 +7,7 @@ class_name Player extends CharacterBody2D
 @onready var move_component: MovementComponent = %MovementComponent
 @onready var animation_component: AnimationComponent = %AnimationComponent
 #endregion Componets
-
+var health: float = 6
 func _ready() -> void:
 	state_machine.init(self)
 
@@ -20,3 +20,8 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
 	move_and_slide()
+func take_damage(weapon_damage: float):
+	$NightGuy/animationplayer.play("take_damage")
+	health -= weapon_damage
+	if health <= 0.0:
+		queue_free()
